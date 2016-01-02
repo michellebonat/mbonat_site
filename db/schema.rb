1280016e02a2c79f2a6561d12a1c6eecf650c89e
michellebonat <michelle.bonat@gmail.com>
2016-01-02 13:45:37 -0800

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102214500) do
+ActiveRecord::Schema.define(version: 20160102214506) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160102214500) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
