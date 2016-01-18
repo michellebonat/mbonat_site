8833814a4d2e3f1b9cff0385d24cdf2794d5d23e
michellebonat <michelle.bonat@gmail.com>
2016-01-18 10:04:37 -0800

    Generate controller, add routes, some views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,7 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
   resources :users
 
