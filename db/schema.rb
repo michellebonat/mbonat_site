e3e55743548f42b51abd14c2a40c5a6b1852c220
michellebonat <michelle.bonat@gmail.com>
2016-01-18 10:35:59 -0800

    Create Post model, update views with dates and posts list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117184055) do
+ActiveRecord::Schema.define(version: 20160118180531) do
 
   create_table "blogit_comments", force: :cascade do |t|
     t.string   "name",       null: false
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20160117184055) do
   end
 
   add_index "blogit_posts", ["blogger_type", "blogger_id"], name: "index_blogit_posts_on_blogger_type_and_blogger_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
