642bb44238e528e2b26374293f9bc7acae156d2c
michellebonat <michelle.bonat@gmail.com>
2016-01-18 10:04:37 -0800

    Generate controller, add routes, some views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   before_filter :authenticate, :except => [ :index, :show ]
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20160118180531) do
 
   create_table "posts", force: :cascade do |t|
