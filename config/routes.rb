39628d394973274a0a7a7091c60f5438ca0c31f2
michellebonat <michelle.bonat@gmail.com>
2016-01-19 13:08:50 -0800

    Add resume page and connect to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-
+  # root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :posts
+
 
   root to: 'visitors#index'
   get 'resume', to: 'visitors#resume'
