7f923bae05343ada698e20621ea60cdde61baadf
michellebonat <michelle.bonat@gmail.com>
2016-02-11 10:52:24 -0800

    Rebase on master and update routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   # root to: 'visitors#index'
   devise_for :users
   resources :users
-
+  resources :posts
 
   root to: 'visitors#index'
   get 'resume', to: 'visitors#resume'
-  
-  resources :posts
+
 
 end
