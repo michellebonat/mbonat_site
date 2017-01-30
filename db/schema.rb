e1c72a5e5b4aebff0b8fe56b4515f6388fe91d65
michellebonat <michelle.bonat@gmail.com>
2017-01-30 14:22:30 -0800

    Add SEO columns to Posts: meta title, meta description, permalink, no index.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604225051) do
+ActiveRecord::Schema.define(version: 20170130221627) do
 
   create_table "blogit_comments", force: :cascade do |t|
     t.string   "name",       null: false
@@ -48,8 +48,12 @@ ActiveRecord::Schema.define(version: 20160604225051) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "meta_title"
+    t.text     "meta_description"
+    t.string   "permalink"
+    t.boolean  "no_index"
   end
 
   create_table "taggings", force: :cascade do |t|
