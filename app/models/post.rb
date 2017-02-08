89d2697c248e634dc5fea47f5bb17269e5c2cbd2
michellebonat <michelle.bonat@gmail.com>
2017-02-07 16:32:32 -0800

    Add ability to update url when changing permalink after original post was created.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :permalink, use: [:slugged, :history, :finders]
   validates :permalink, presence: true, uniqueness: true
+
+  def should_generate_new_friendly_id?
+    permalink_changed?
+  end
 end
