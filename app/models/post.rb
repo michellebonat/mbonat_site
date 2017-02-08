a27736899ef311241ab38f7a7e0d5170edcb7e9a
michellebonat <michelle.bonat@gmail.com>
2017-02-07 16:20:32 -0800

    Add finders on the Post model so friendly ID can find the post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :permalink, use: [:slugged, :history]
+  friendly_id :permalink, use: [:slugged, :history, :finders]
   validates :permalink, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207235321) do
+ActiveRecord::Schema.define(version: 20170208001323) do
 
   create_table "blogit_comments", force: :cascade do |t|
     t.string   "name",       null: false
@@ -44,6 +44,19 @@ ActiveRecord::Schema.define(version: 20170207235321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
