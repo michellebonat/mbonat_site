4715d124d3fb8bbf1347edcbb694d6e1e35f113d
michellebonat <michelle.bonat@gmail.com>
2017-03-07 11:50:37 -0800

    Create portfolios infrastructure: controller and methods.

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,22 @@
+class PortfolioController < ApplicationController
+  def data_simply
+  end
+
+  def my_money_butler
+  end
+
+  def ruby_rails_portfolio
+  end
+
+  def wikis
+  end
+
+  def seo_ruby_rails
+  end
+
+  def oracle_financial_apps
+  end
+
+  def fintechsandbox
+  end
+end
