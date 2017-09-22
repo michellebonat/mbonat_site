5bf003230842ce2a4d7e2288277378a742ec180d
michellebonat <michelle.bonat@gmail.com>
2017-09-22 12:56:17 -0700

    New method in Visitors controller creates instance variable to display most recent post

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,9 @@
 class VisitorsController < ApplicationController
+
+  def index
+    @posts = Post.order('created_at DESC').limit(1)
+  end
+
   def resume
     @meta_title = "Software engineer full stack ruby rails Michelle Bonat"
   end
