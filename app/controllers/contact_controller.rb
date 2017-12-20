e3edf1ff8bd8add1a94a10b24911339a23531787
michellebonat <michelle.bonat@gmail.com>
2017-12-20 14:46:57 -0800

    Update new Contact controller to define the instance variable and add a create method that redirects to root if successful or returns a helpful error message with alternative contact info.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,18 @@
 class ContactController < ApplicationController
+
   def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+      redirect_to root_path, notice: 'Your message has been sent successfully'
+    else
+      flash.now[:error] = 'Cannot send message. Please try again, contact me on Twitter at @mbonat, or send a carrier pigeon.'
+      render :new
+    end
   end
 end
