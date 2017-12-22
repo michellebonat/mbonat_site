8a558db5aa454faa1af73fa92e26e53cecbb32f8
michellebonat <michelle.bonat@gmail.com>
2017-12-22 11:30:52 -0800

    Remove captcha validation to get form working first; add twitter contact info and link on form page as alternative.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,6 @@ class Contact < MailForm::Base
   attribute :name, :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
-  attribute :nickname,  :captcha  => true
 
   def headers
     {
